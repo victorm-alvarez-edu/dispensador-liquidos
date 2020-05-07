7b26fe0d93d39b1da47e7714e4fb748b9bfadeb3
victorm-alvarez-edu <victorm.alvarez.edu@gmail.com>
2020-05-08 00:45:49 +0200

    Update Dispensador.liquidos.sensor.dixital.ino
    Versión operativa para o IR FC-51, con activación de relé durante un tempo e periodo de espera antes de volver a estar o dispositivo dispoñible para dispensar unha segunda vez.

--- Dispensador.liquidos.sensor.dixital/Dispensador.liquidos.sensor.dixital.ino
+++ Dispensador.liquidos.sensor.dixital/Dispensador.liquidos.sensor.dixital.ino
@@ -23,9 +23,9 @@
 #define PIR 13
 #define RELAY 8
 
-int tDispensador = 1000;  // Tempo que está ON o dispensador
-int tEspera = 200;        // Tempo de espera antes de suministrar
-unsigned long int tInicial = 0;
+int tDispensador = 3;  // Tempo en segundos que está ON o dispensador
+int tEspera = 10;      // Tempo en segundos de espera antes de suministrar
+unsigned long int tInicial = 0; // Importante: unsigned long int evita desbordamento
 bool tempoCumplido = true;
 bool pir = false;         // Estado do sensor de presenza
 
@@ -34,23 +34,36 @@ void setup() {
   pinMode(PIR, INPUT);
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
+  tInicial = millis();
   Serial.begin(9600);
 }
 
 // -------- Lóxica do dispensador --------
 void loop() {
   // Lectura do sensor de presenza
-  pir = digitalRead(PIR);
+  pir = !digitalRead(PIR);
   // Se o sensor está activo e pasou tempo suficiente
-  if(pir && tempoCumplido) {  
+  if(pir && tempoCumplido) {
+    Serial.print("Estado sensor IR: "); Serial.println(pir);
+    Serial.print("Tempo cumplido: "); Serial.println(tempoCumplido);
     digitalWrite(RELAY, HIGH);  // ... activa dispensador
+    Serial.println("Relé ON");
     tInicial = millis();        // ... empeza temporización
   }
   //Se pasou o tempo que necesita o mecanismo dispensador...
-  else if(millis() - tInicial >= tDispensador) {
+  if(millis() - tInicial >= tDispensador * 1000) {
     digitalWrite(RELAY, LOW);  //  Apagamos dispensador
-    tInicial = 0;              //  Reiniciamos tempo actual
+    Serial.println("Relé OFF");
   }
   // Engadimos unha pequena espera antes de habilitar dispensador
-  tempoCumplido = millis() - tInicial >= tDispensador + tEspera;
+  tempoCumplido = millis() - tInicial >= (tDispensador + tEspera) * 1000;
 }
+
+/*
+ * Notas:
+ * (a) O sensor IR de proximidade FC-51 dá saída LOW cando detecta presenza,
+ *     por iso lemos o sensor co valor negado con '!';
+ * (b) Algúns relés con dous LEDs (vermello/verde) poden indicar: vermello, relé
+ *     alimentado e verde, relé OFF (LED verde apagado, relé ON, con consumo por
+ *     estar activo).
+ */
