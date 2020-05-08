aaf8d875f3e6717530feee2cd0f23ab519ab009e
victorm-alvarez-edu <victorm.alvarez.edu@gmail.com>
2020-05-08 12:32:19 +0200

    Corrixidos erros de programación e asegurada a operatividade para sensores dixitais.

--- Dispensador.liquidos/Dispensador.liquidos.ino
+++ Dispensador.liquidos/Dispensador.liquidos.ino
@@ -29,9 +29,9 @@
 #define RELAY 8
 #define B36 A0  // Entrada para o PIR analóxico
 
-int tDispensador = 1000;  // Tempo que está ON o dispensador
-int tEspera = 200;        // Tempo de espera antes de suministrar
-unsigned long int tInicial = 0;
+int tDispensador = 3;  // Tempo en segundos que está ON o dispensador
+int tEspera = 10;      // Tempo en segundos de espera antes de suministrar
+unsigned long int tInicial = 0; // Importante: unsigned long int evita desbordamento
 bool tempoCumplido = true;
 bool pir = false;         // Estado do sensor de presenza
 bool b36 = false;         // Estado do sensor analóxico
@@ -43,24 +43,31 @@ void setup() {
   pinMode(PIR, INPUT);
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
+  tInicial = millis();
   Serial.begin(9600);
 }
 
 // -------- Lóxica do dispensador --------
 void loop() {
   // Lectura do sensor de presenza
-  pir = digitalRead(PIR);
-  b36 = map(analogRead(A0), 0, 1023, 500, 0) > umbralB36 * 100;
+  pir = !digitalRead(PIR);
+  b36 = map(analogRead(A0), 0, 1023, 500, 0) > umbralB36 * 100; // Non estamos seguros de como funciona, queda pendente
   // Se o sensor está activo e pasou tempo suficiente
-  if((pir && tempoCumplido) || (b36 && tempoCumplido)) {  
+  if((pir && tempoCumplido)) {  // || (b36 && tempoCumplido)) {  // Queda pendente
     digitalWrite(RELAY, HIGH);  // ... activa dispensador
     tInicial = millis();        // ... empeza temporización
   }
   //Se pasou o tempo que necesita o mecanismo dispensador...
-  else if(millis() - tInicial >= tDispensador) {
+  else if(millis() - tInicial >= tDispensador * 1000) {
     digitalWrite(RELAY, LOW);  //  Apagamos dispensador
-    tInicial = 0;              //  Reiniciamos tempo actual
   }
   // Engadimos unha pequena espera antes de habilitar dispensador
-  tempoCumplido = millis() - tInicial >= tDispensador + tEspera;
+  tempoCumplido = millis() - tInicial >= (tDispensador + tEspera) * 1000;
 }
+
+/*
+ * Notas:
+ * Queda pendente a comprobación do sensor de presenza Allnet 4duino B36 até que poda 
+ * facer probas fisicamente co sensor. Nas que temos feito, non queda claro como 
+ * funciona. Parece que é un sensor analóxico, pero tampouco está claro.
+ */
